3b4875129fb63ffc6d0485d3881cb3a3649109b9
WictorFeitosa <wictor.feitosa@gmail.com>
2022-07-21 00:06:12 -0300

    pagina de carrinho, adicionar produtos, remover produtos, fazer requisição de compra de produto

--- src/pages/ShoppingCart/ShoppingCartPage.js
+++ src/pages/ShoppingCart/ShoppingCartPage.js
@@ -1,14 +1,51 @@
-import React from "react";
-import {exemplo} from "./styledShoppingCartPage"
+import React, { useContext, useState } from "react";
+import { exemplo } from "./styledShoppingCartPage";
 import { useProtected } from "../../hooks/useProtected";
+import { GlobalContext } from "../../global/GlobalContext";
+import { navigate, useNavigate } from "react-router-dom";
 
+export default function CarrinhoPage() {
+  const navigate = useNavigate();
 
-export default function CarrinhoPage () {
+  const { states, setters, functions } = useContext(GlobalContext);
+  const { restaurants, cart } = states;
+  const { setRestaurants, setCart } = setters;
+  const  {addToCart, removeToCart, postOrder} = functions 
+  
+  useProtected();
+  
+  //============= soma o valor total dos itens no carrinho
+  let totalPrice = 0
+  for (const i of cart) {
+    totalPrice += (i.price * i.quantity)
+   console.log(totalPrice)
+  }
 
-  useProtected()
+console.log(cart)
+
   return (
     <div>
-        Carrinho Page
+      <h1>CARRINHO PAGE</h1>
+      <button onClick={() => navigate("/feed")}>feed</button>
+      {cart &&
+        cart.map((item) => {
+          // setTotal(total+item.total)
+          return( 
+            <div>
+              <div>{item.name}</div>
+              <img width="200px" src={item.photoUrl}/>
+              <button onClick={()=>addToCart(item)}>MAIS</button>
+              {item.quantity}
+              <button onClick={()=>removeToCart(item)}>MENOS</button>  
+              <br/>            
+              R${item.price}          
+
+            </div>
+            )
+        })}
+            Total: {totalPrice}
+            <button onClick={()=>postOrder()}>postOrder</button>
+            
     </div>
   );
 }
